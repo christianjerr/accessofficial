e8bc5cb64df861871210175ff103d77423fe2880
Christian Jerr T. Buarao <44043066+christianjerr@users.noreply.github.com>
2019-04-15 13:58:46 +0800

    Add files via upload

---
+++ precache-manifest.3b4639eb61588ff89ebb88af39d499b9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d911c99b1f249117243a69042097c445",
+    "url": "/accessofficial/static/media/shs.d911c99b.jpg"
+  },
+  {
+    "revision": "d80af6a9111bda0ab84b",
+    "url": "/accessofficial/static/css/main.a43aa28f.chunk.css"
+  },
+  {
+    "revision": "6259db70750f542fc1f1",
+    "url": "/accessofficial/static/js/1.6259db70.chunk.js"
+  },
+  {
+    "revision": "b115e2a223922db2ed35",
+    "url": "/accessofficial/static/js/runtime~main.b115e2a2.js"
+  },
+  {
+    "revision": "74040eb522498327ed228a42d09d663c",
+    "url": "/accessofficial/static/media/access.74040eb5.png"
+  },
+  {
+    "revision": "d80af6a9111bda0ab84b",
+    "url": "/accessofficial/static/js/main.d80af6a9.chunk.js"
+  },
+  {
+    "revision": "aedbd471f663cbe5f8f4a5966f0eeed5",
+    "url": "/accessofficial/static/media/1.aedbd471.pdf"
+  },
+  {
+    "revision": "0a52b068a0384c1f8019380abe3fd5a1",
+    "url": "/accessofficial/static/media/3.0a52b068.pdf"
+  },
+  {
+    "revision": "bc18c5573aaff8d3b3a4c498e5ef19a8",
+    "url": "/accessofficial/static/media/4.bc18c557.pdf"
+  },
+  {
+    "revision": "227a075290d193eb52edb97daf545f15",
+    "url": "/accessofficial/static/media/banner.227a0752.png"
+  },
+  {
+    "revision": "3426d6ccc0ffffa202b8842f7af9284b",
+    "url": "/accessofficial/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/accessofficial/precache-manifest.3b4639eb61588ff89ebb88af39d499b9.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/accessofficial/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
